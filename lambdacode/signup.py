fc3daaf69c078fee71d17bf79e7443cebd43713d
Nishanth7076 <x22103945@student.ncirl.ie>
2023-04-18 18:10:35 -0600

    Added Bandit

--- lambdacode/signup.py
+++ lambdacode/signup.py
@@ -1,9 +1,8 @@
-
+'''This file is used for signing up customers'''
 import json
 import random
 import string
 import boto3
-from boto3.dynamodb.conditions import Key
 from boto3 import client as boto3_client
 
 client = boto3.resource('dynamodb')
@@ -11,6 +10,7 @@ dynamodb = boto3.client('dynamodb')
 lambda_client = boto3_client('lambda')
 
 def lambda_handler(event):
+    "Function for handling signup event"
     body = json.loads(event['body'])
     firstname = body['firstname']
     lastname = body['lastname']
@@ -24,9 +24,7 @@ def lambda_handler(event):
     card_num = "0611" + ''.join(random.choice(string.digits) for _ in range(12))
     expiry = str(random.randrange(1,13)).zfill(2)+ str(random.randrange(25,30))
     cvv = str(random.randrange(1,1000)).zfill(3)
-    
     dynamodb.put_item(TableName='UsersInfo', Item={'type':{'S':'user'},'email':{'S': email}, 'name': {'S': name}, 'pincode': {'S': pincode}, 'accountNumber': {'S': account_num }, 'balance': {'S': balance}, 'cvv': {'S': cvv}, 'cardNumber': {'S': card_num}, 'expiry': {'S': expiry}, 'onlineTransactions': {'S': online_transaction}, 'password': {'S': password}})
-
     msg = {"mailType": "signup", "email": email, "password": password, "name": name}
     lambda_client.invoke(FunctionName="TestMail",
                                            InvocationType='Event',
